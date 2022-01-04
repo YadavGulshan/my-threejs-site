1e3c215d332918b5ee583f7381616e7c36044f83
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-04 18:15:28 +0530

    idk

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,38 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Three = __importStar(require("three"));
-var scene = new Three.Scene();
-var camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new Three.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+// Set the background color of the scene
+renderer.setClearColor("#e6e6e6", 1);
+// Setting up responsive canvas
+window.addEventListener('resize', function () {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    // Since camera is a perspective camera, we need to update the camera's projection matrix
+    camera.updateProjectionMatrix();
+});
 document.body.appendChild(renderer.domElement);
-const geometry = new Three.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+//  Mesh info
+//  What is a mesh?
+//  A mesh is a 3D object that is made up of vertices, faces, and edges.
+const cube = new THREE.Mesh(geometry, material);
+// Add cube into the scene
+scene.add(cube);
+// Camera info
+// What is a camera?
+// A camera is a device that can be used to view a scene.
+camera.position.z = 5;
+// Lets animate the cube
+function animate() {
+    requestAnimationFrame(animate);
+    // cube.rotation.x += 0.1;
+    // //  or
+    cube.rotateX(0.1);
+    cube.rotateY(0.1);
+    // Render the scene
+    renderer.render(scene, camera);
+}
+animate();
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,10 +1,51 @@
-import * as Three from 'three';
-var scene = new Three.Scene();
-var camera = new Three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new Three.WebGLRenderer();
-
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({antialias: true});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Set the background color of the scene
+renderer.setClearColor("#e6e6e6",1);
+
+
+// Setting up responsive canvas
+window.addEventListener('resize', function(){
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    // Since camera is a perspective camera, we need to update the camera's projection matrix
+    camera.updateProjectionMatrix();
+})
 document.body.appendChild(renderer.domElement);
 
-const geometry = new Three.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+//  Mesh info
+//  What is a mesh?
+//  A mesh is a 3D object that is made up of vertices, faces, and edges.
+const cube = new THREE.Mesh(geometry, material);
+
+// Add cube into the scene
+scene.add(cube);
+
+// Camera info
+// What is a camera?
+// A camera is a device that can be used to view a scene.
+camera.position.z = 5;
+
+
+// Lets animate the cube
+function animate(){
+    requestAnimationFrame(animate);
+    // cube.rotation.x += 0.1;
+    // //  or
+    cube.rotateX(0.1);
+    cube.rotateY(0.1);
+
+
+    // Render the scene
+    renderer.render(scene, camera);
+}
+
+animate();
